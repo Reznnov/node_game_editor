a83bff047021052bb0afe1ae8485c9ca74c85f90
Reznnov <64481216+Reznnov@users.noreply.github.com>
2024-01-03 00:42:47 +0300

    Refactoring Window, Menu and Actions
    очень много изменений... создал экземпляр главной сцены, надо фиксить кнопки

---
+++ examples/calc_window.py
@@ -0,0 +1,162 @@
+import os
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+from nodeeditor.utils import loadStylesheets
+from nodeeditor.node_editor_window import NodeEditorWindow
+from examples.calc_sub_window import CalculatorSubWindow
+from nodeeditor.utils import dumpException
+
+# images for the dark skin
+import examples.qss.nodeeditor_dark_resources
+
+class CalculatorWindow(NodeEditorWindow):
+
+    def initUI(self):
+        self.name_company = 'Blenderfreak'
+        self.name_product = 'Calculator NodeEditor'
+
+        self.stylesheet_filename = os.path.join(os.path.dirname(__file__), "qss/nodeeditor.qss")
+        loadStylesheets(
+            os.path.join(os.path.dirname(__file__), "qss/nodeeditor-dark.qss"),
+            self.stylesheet_filename
+        )
+
+        self.mdiArea = QMdiArea()
+        self.mdiArea.setHorizontalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+        self.mdiArea.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+        self.mdiArea.setViewMode(QMdiArea.TabbedView)
+        self.mdiArea.setDocumentMode(True)
+        self.mdiArea.setTabsClosable(True)
+        self.mdiArea.setTabsMovable(True)
+        self.setCentralWidget(self.mdiArea)
+
+        self.mdiArea.subWindowActivated.connect(self.updateMenus)
+        self.windowMapper = QSignalMapper(self)
+        self.windowMapper.mapped[QWidget].connect(self.setActiveSubWindow)
+
+        self.createActions()
+        self.createMenus()
+        self.createToolBars()
+        self.createStatusBar()
+        self.updateMenus()
+
+        self.createNodesDock()
+
+        self.readSettings()
+
+        self.setWindowTitle("Main scene Example")
+
+    def closeEvent(self, event):
+        self.mdiArea.closeAllSubWindows()
+        if self.mdiArea.currentSubWindow():
+            event.ignore()
+        else:
+            self.writeSettings()
+            event.accept()
+
+    def updateMenus(self):
+        pass
+
+    def createActions(self):
+        super().createActions()
+
+        self.closeAct = QAction("Cl&ose", self, statusTip="Close the active window", triggered=self.mdiArea.closeActiveSubWindow)
+        self.closeAllAct = QAction("Close &All", self, statusTip="Close all the windows", triggered=self.mdiArea.closeAllSubWindows)
+        self.tileAct = QAction("&Tile", self, statusTip="Tile the windows", triggered=self.mdiArea.tileSubWindows)
+        self.cascadeAct = QAction("&Cascade", self, statusTip="Cascade the windows", triggered=self.mdiArea.cascadeSubWindows)
+        self.nextAct = QAction("Ne&xt", self, shortcut=QKeySequence.NextChild, statusTip="Move the focus to the next window", triggered=self.mdiArea.activateNextSubWindow)
+        self.previousAct = QAction("Pre&vious", self, shortcut=QKeySequence.PreviousChild, statusTip="Move the focus to the previous window", triggered=self.mdiArea.activatePreviousSubWindow)
+
+        self.separatorAct = QAction(self)
+        self.separatorAct.setSeparator(True)
+
+        self.aboutAct = QAction("&About", self, statusTip="Show the application's About box", triggered=self.about)
+
+    def onFileNew(self):
+        try:
+            subwnd = self.createMdiChild()
+            subwnd.show()
+        except Exception as e: dumpException(e)
+
+    def about(self):
+        QMessageBox.about(self, "Main scene Example",
+                "The <b>NN engine</b> example demonstrates how to easy create your feture vizual novel "
+                "document interface applications using PyQt5 and NodeEditor. For more information visit: "
+                "<a href='https://discord.gg/A6vAhdBX/'>discord</a>")
+
+    def createMenus(self):
+        super().createMenus()
+
+        self.windowMenu = self.menuBar().addMenu("&Window")
+        self.updateWindowMenu()
+        self.windowMenu.aboutToShow.connect(self.updateWindowMenu)
+
+        self.menuBar().addSeparator()
+
+        self.helpMenu = self.menuBar().addMenu("&Help")
+        self.helpMenu.addAction(self.aboutAct)
+
+    def updateWindowMenu(self):
+        self.windowMenu.clear()
+        self.windowMenu.addAction(self.closeAct)
+        self.windowMenu.addAction(self.closeAllAct)
+        self.windowMenu.addSeparator()
+        self.windowMenu.addAction(self.tileAct)
+        self.windowMenu.addAction(self.cascadeAct)
+        self.windowMenu.addSeparator()
+        self.windowMenu.addAction(self.nextAct)
+        self.windowMenu.addAction(self.previousAct)
+        self.windowMenu.addAction(self.separatorAct)
+
+        windows = self.mdiArea.subWindowList()
+        self.separatorAct.setVisible(len(windows) != 0)
+
+        for i, window in enumerate(windows):
+            child = window.widget()
+
+            text = "%d %s" % (i + 1, child.getUserFriendlyFilename())
+            if i < 9:
+                text = '&' + text
+
+            action = self.windowMenu.addAction(text)
+            action.setCheckable(True)
+            action.setChecked(child is self.activeMdiChild())
+            action.triggered.connect(self.windowMapper.map)
+            self.windowMapper.setMapping(action, window)
+
+    def createToolBars(self):
+        pass
+
+    def createNodesDock(self):
+        self.listWidget = QListWidget()
+        self.listWidget.addItem("Add scene")
+        self.listWidget.addItem("Add choice scene")
+        self.listWidget.addItem("Add script")
+        self.listWidget.addItem("Add variable")
+
+        self.items = QDockWidget("Nodes")
+        self.items.setWidget(self.listWidget)
+        self.items.setFloating(False)
+
+        self.addDockWidget(Qt.RightDockWidgetArea, self.items)
+
+    def createStatusBar(self):
+        self.statusBar().showMessage("Ready")
+
+    def createMdiChild(self):
+        nodeeditor = CalculatorSubWindow()
+        subwnd = self.mdiArea.addSubWindow(nodeeditor)
+        return subwnd
+
+    def activeMdiChild(self):
+        """ we're returning NodeEditorWidget here... """
+        activeSubWindow = self.mdiArea.activeSubWindow()
+        if activeSubWindow:
+            return activeSubWindow.widget()
+        return None
+
+    def setActiveSubWindow(self, window):
+        if window:
+            self.mdiArea.setActiveSubWindow(window)
---
+++ nodeeditor/node_editor_widget.py
@@ -0,0 +1,89 @@
+import os
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+from nodeeditor.node_scene import Scene
+from nodeeditor.node_node import Node
+from nodeeditor.node_edge import Edge, EDGE_TYPE_BEZIER
+from nodeeditor.node_graphics_view import QDMGraphicsView
+
+
+class NodeEditorWidget(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.filename = None
+
+        self.initUI()
+
+
+    def initUI(self):
+        self.layout = QVBoxLayout()
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(self.layout)
+
+        # crate graphics scene
+        self.scene = Scene()
+        # self.grScene = self.scene.grScene
+
+        self.addNodes()
+
+        # create graphics view
+        self.view = QDMGraphicsView(self.scene.grScene, self)
+        self.layout.addWidget(self.view)
+
+
+    def isModified(self):
+        return self.scene.has_been_modified
+
+    def isFilenameSet(self):
+        return self.filename is not None
+
+    def getUserFriendlyFilename(self):
+        name = os.path.basename(self.filename) if self.isFilenameSet() else "New Graph"
+        return name + ("*" if self.isModified() else "")
+
+    def addNodes(self):
+        node1 = Node(self.scene, "My Scene 1", inputs=[0,0,0], outputs=[1])
+        node2 = Node(self.scene, "My Scene 2", inputs=[3,3,3], outputs=[1])
+        node3 = Node(self.scene, "My Scene 3", inputs=[2,2,2], outputs=[1])
+        node1.setPos(-350, -250)
+        node2.setPos(-75, 0)
+        node3.setPos(200, -150)
+
+        edge1 = Edge(self.scene, node1.outputs[0], node2.inputs[0], edge_type=EDGE_TYPE_BEZIER)
+        edge2 = Edge(self.scene, node2.outputs[0], node3.inputs[0], edge_type=EDGE_TYPE_BEZIER)
+
+
+    def addDebugContent(self):
+        greenBrush = QBrush(Qt.green)
+        outlinePen = QPen(Qt.black)
+        outlinePen.setWidth(2)
+
+
+        rect = self.grScene.addRect(-100, -100, 80, 100, outlinePen, greenBrush)
+        rect.setFlag(QGraphicsItem.ItemIsMovable)
+
+        text = self.grScene.addText("This is my Awesome text!", QFont("Ubuntu"))
+        text.setFlag(QGraphicsItem.ItemIsSelectable)
+        text.setFlag(QGraphicsItem.ItemIsMovable)
+        text.setDefaultTextColor(QColor.fromRgbF(1.0, 1.0, 1.0))
+
+
+        widget1 = QPushButton("Hello World")
+        proxy1 = self.grScene.addWidget(widget1)
+        proxy1.setFlag(QGraphicsItem.ItemIsMovable)
+        proxy1.setPos(0, 30)
+
+
+        widget2 = QTextEdit()
+        proxy2 = self.grScene.addWidget(widget2)
+        proxy2.setFlag(QGraphicsItem.ItemIsSelectable)
+        proxy2.setPos(0, 60)
+
+
+        line = self.grScene.addLine(-200, -200, 400, -100, outlinePen)
+        line.setFlag(QGraphicsItem.ItemIsMovable)
+        line.setFlag(QGraphicsItem.ItemIsSelectable)
+
